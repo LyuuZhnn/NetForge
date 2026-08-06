190925ba9b342ce816755c8ed204cf0d67316627
TJKT Student <gzberhanif@gmail.com>
2026-08-06 19:57:18 +0700

    NetForge V1

---
+++ login.js
@@ -0,0 +1,21 @@
+function login(){
+
+const username=document.getElementById("username").value.trim();
+const kelas=document.getElementById("kelas").value.trim();
+const role=document.getElementById("role").value;
+
+if(username===""||kelas===""){
+
+alert("Username dan Kelas wajib diisi!");
+
+return;
+
+}
+
+localStorage.setItem("netforge_username",username);
+localStorage.setItem("netforge_kelas",kelas);
+localStorage.setItem("netforge_role",role);
+
+window.location.href="index.html";
+
+}
